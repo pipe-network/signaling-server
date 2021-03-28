309723cd915be339b79e792b57eec8f44f94cbe4
Michael Bykovski <michael.bykovskii@aoe.com>
2021-03-28 14:07:52 +0200

    Adapt port as flag and cert.crt and cert.key

--- application/main_application.go
+++ application/main_application.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"flag"
+	"fmt"
 	"github.com/pipe-network/signaling-server/interface/controllers"
 	"log"
 	"net/http"
@@ -20,16 +21,18 @@ func NewMainApplication(
 }
 
 func (a *MainApplication) Run() {
-	address := flag.String("address", "localhost:8080", "http service address")
-	tlsCertFilePath := flag.String("tls_cert_file", "./cert.pem", "TLS certificate file path")
-	tlsKeyFilePath := flag.String("tls_key_file", "./key.pem", "TLS key file path")
+	address := flag.String("address", "localhost", "http service host")
+	port := flag.Int("port", 8080, "http service port")
+	tlsCertFilePath := flag.String("tls_cert_file", "./cert.crt", "TLS certificate file path")
+	tlsKeyFilePath := flag.String("tls_key_file", "./cert.key", "TLS key file path")
+
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/", a.SignallingController.WebSocket)
-	log.Printf("Running on: https://%s", *address)
+	log.Printf("Running on: https://%s:%d", *address, *port)
 	log.Fatal(
 		http.ListenAndServeTLS(
-			*address,
+			fmt.Sprintf("%s:%d", *address, *port),
 			*tlsCertFilePath,
 			*tlsKeyFilePath,
 			nil,
